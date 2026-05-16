edb53f32ba2b0b7c3e374b3e8fb868a405812aed
ROHAN TIWARI <87462011+rohangit-hub@users.noreply.github.com>
2026-05-16 14:30:36 +0530

    cors config

--- server/index.js
+++ server/index.js
@@ -17,7 +17,11 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
 
 
 import dotenv from "dotenv";
